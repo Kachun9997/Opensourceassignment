e30c6b7688d9b528041ea145753b99b9437a2529
Kachun9997 <chooikachun@gmail.com>
2019-07-06 22:22:01 +0800

    Update jquery.richtext.js

--- Extension 1 sticky_notes-1.5-an+fx/popup/jquery.richtext.js
+++ Extension 1 sticky_notes-1.5-an+fx/popup/jquery.richtext.js
@@ -1643,6 +1643,29 @@
             createSelection(selection.node, selection.start, selection.end);
             return selection;
         }
+       /**
+         * Create selection on node element
+         * @param {Node} field
+         * @param {int} start
+         * @param {int} end
+         **/ 
+        function createSelection(field, start, end) {
+            if( field.createTextRange ) {
+                var selRange = field.createTextRange();
+                selRange.collapse(true);
+                selRange.moveStart('character', start);
+                selRange.moveEnd('character', end);
+                selRange.select();
+                field.focus();
+            } else if( field.setSelectionRange ) {
+                field.focus();
+                field.setSelectionRange(start, end);
+            } else if( typeof field.selectionStart != 'undefined' ) {
+                field.selectionStart = start;
+                field.selectionEnd = end;
+                field.focus();
+            }
+        }
         }
 
     };
