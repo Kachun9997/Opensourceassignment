538971d051753e224afe664200ac613fc72d3795
fishyjefft <34062588+fishyjefft@users.noreply.github.com>
2019-07-09 15:23:45 +0800

    Update main.js

--- Extension 3 Reminder/__MACOSX/main.js
+++ Extension 3 Reminder/__MACOSX/main.js
@@ -1,11 +1,16 @@
-var audio = new Audio("alarm.mp3");
+var audio = new Audio('submarine-diving-alarm-daniel_simon.mp3');
 
 browser.alarms.onAlarm.addListener(alarm => {
-  audio.play();
-  browser.notifications.create(alarm.name, {
-    type: "basic",
-    iconUrl: "alarm.png",
-    title: "Remainder",
-    message: alarm.name
-  });
-});
+  const items = browser.storage.local.get('tsl_reminder');
+
+  items.then(res => {
+    audio.play();
+
+    var splitted = alarm.name.split(',');
+
+    browser.notifications.create(splitted[0], {
+      type: 'basic',
+      iconUrl: 'alarm.png',
+      title: 'Remainder',
+      message: splitted[0]
+    });
