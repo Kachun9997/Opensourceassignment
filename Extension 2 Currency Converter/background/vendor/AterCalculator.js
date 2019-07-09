2332ec97be5954a0a4d08d60704ae25b12f8c7f9
fishyjefft <34062588+fishyjefft@users.noreply.github.com>
2019-07-09 16:21:44 +0800

    Update AterCalculator.js

--- Extension 2 Currency Converter/background/vendor/AterCalculator.js
+++ Extension 2 Currency Converter/background/vendor/AterCalculator.js
@@ -256,6 +256,16 @@ var AterCalculator = (function () {
             length,
             j,
             l;
+        
+        for (i = 0, length = tokens.length; i < length; i++) {
+            token = tokens[i];
+            if (typeof (operator = operators[token] || functions[token]) !== 'undefined') {
+                if (resultStack.length < operator.operands) {
+                    throw 'Insufficient values in the expression.';
+                }
+                for (l = resultStack.length, j = l - operator.operands; j < l; j++) {
+                    args.push(resultStack[j]);
+                }
 
         
 
